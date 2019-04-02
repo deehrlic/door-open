e7f23512e9edca9c10a7959a2cea2cad2075d4e6
deehrlic <47901184+deehrlic@users.noreply.github.com>
2019-04-01 18:31:18 -0700

    Removing Files
    Not needed for this branch.

--- RemotePi/mqtt_publisher.py
+++
@@ -1,7 +0,0 @@
-import paho.mqtt.publish as publish
- 
-#IP OF MACHINE YOU ARE SENDING TO GOES HERE, CHANGE IF USING
-MQTT_SERVER = "YOUR.IP.HERE"
-MQTT_PATH = "test_channel"
- 
-publish.single(MQTT_PATH, "opendoor", hostname=MQTT_SERVER)
